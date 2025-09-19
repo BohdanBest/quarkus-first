d9fa8ea5e17cdbe24cb119e4f26976a0a2886c50
BohdanBest <bohdanbohdan32@gmail.com>
2025-09-19 10:55:12 +0300

    fixed problem with keycloak connect

--- src/main/java/com/bohdanbest/SecuredResource.java
+++ src/main/java/com/bohdanbest/SecuredResource.java
@@ -1,4 +1,18 @@
 package com.bohdanbest;
 
+import io.quarkus.security.Authenticated;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/secured")
+@Authenticated
 public class SecuredResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getSecuredResource() {
+        return "This is a secured resource!";
+    }
 }
